2d52bc4619a770a97c4912f190dd7fcfe86aa370
Joel Dixon <joel.dixon@ni.com>
2025-09-28 12:22:04 -0500

    Introduce a composite action to check a test project and expect errors conditionally

--- .github/test_projects/mypy-errors/src/test_project/mypy-errors.py
+++ .github/test_projects/mypy-errors/src/test_project/mypy-errors.py
@@ -1,7 +1,7 @@
 """Required docstring."""
 
 
-def foo(x: int, y: int):
+def foo(x: int, y: int): # mypy error: Function is missing a return type annotation
     """Required docstring."""
     print("Hello, world!")
     return x + y
