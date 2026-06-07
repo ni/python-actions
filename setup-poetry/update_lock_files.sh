5bc8fde4a283f250c6f1c2a39f886327a6a8ebe4
Brad Keryan <brad.keryan@ni.com>
2026-06-07 18:40:17 -0500

    setup-poetry: Use requirements.txt format instead of pylock.toml

--- setup-poetry/update_lock_files.sh
+++ setup-poetry/update_lock_files.sh
@@ -6,6 +6,8 @@
 for i in versions/*; do
     pushd $i
     poetry lock
-    poetry export -f pylock.toml -o pylock.toml
+    # After we drop Python 3.9, consider switching to pylock.toml format.
+    # (pip 26.1 added pylock.toml support, but it also dropped Python 3.9 support.)
+    poetry export -o requirements.txt
     popd
 done
